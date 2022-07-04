1a7c51e25fe7a2c97f83338edd592293b1508aaa
ekkifirdiansyah <73180261+ekkifirdiansyah@users.noreply.github.com>
2022-07-04 21:10:35 +0700

    setting url CloudDB

--- database/db.js
+++ database/db.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/bookingDB", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://alpha01:f21Gsatof7Zn7Sgn@project01.x7f8bhw.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      }
+    );
 
     console.log(`MongoDB connected : ${conn.connection.host}`);
   } catch (error) {
